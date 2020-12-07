a63444ae0e8f55cc569d34919b35ded844d37244
Jack Whitham <jack.d.whitham@gmail.com>
2020-12-07 13:14:29 +0000

    small improvements

--- 6/question_counter.vhdl
+++ 6/question_counter.vhdl
@@ -6,15 +6,17 @@ entity question_counter is
     generic (NUMBER_OF_QUESTIONS    : Integer;
              COUNT_WIDTH            : Integer);
     port (input_question            : in std_logic_vector (1 to NUMBER_OF_QUESTIONS);
-          input_valid               : in std_logic;
+          end_of_line               : in std_logic;
+          end_of_group              : in std_logic;
           output_value              : out unsigned (COUNT_WIDTH - 1 downto 0); 
           clock                     : in std_logic;
           reset                     : in std_logic);
 end question_counter;
 
 architecture behaviour of question_counter is
-    signal counter          : unsigned (COUNT_WIDTH - 1 downto 0);
-    signal new_value        : unsigned (COUNT_WIDTH - 1 downto 0);
+    signal total_for_file   : unsigned (COUNT_WIDTH - 1 downto 0);
+    signal total_for_group  : unsigned (COUNT_WIDTH - 1 downto 0);
+    signal union_for_group  : std_logic_vector (1 to NUMBER_OF_QUESTIONS);
 
     component set_population is
         generic (NUMBER_OF_QUESTIONS    : Integer;
@@ -25,21 +27,27 @@ architecture behaviour of question_counter is
 
 begin
 
-    output_value <= counter;
+    output_value <= total_for_file;
     
     pop : set_population
         generic map (NUMBER_OF_QUESTIONS => NUMBER_OF_QUESTIONS,
                      COUNT_WIDTH => COUNT_WIDTH)
-        port map (input_question => input_question,
-                  output_value => new_value);
+        port map (input_question => union_for_group,
+                  output_value => total_for_group);
 
     process (clock) is
     begin
         if clock = '1' and clock'event then
             if reset = '1' then
-                counter <= (others => '0');
-            elsif input_valid = '1' then
-                counter <= counter + new_value;
+                total_for_file <= (others => '0');
+                union_for_group <= (others => '0');
+            else
+                if end_of_group = '1' then
+                    total_for_file <= total_for_file + total_for_group;
+                    union_for_group <= (others => '0');
+                elsif end_of_line = '1' then
+                    union_for_group <= union_for_group or input_question;
+                end if;
             end if;
         end if;
     end process;
--- 6/test.vhdl
+++ 6/test.vhdl
@@ -1,19 +1,18 @@
---  Hello world program
-use std.textio.all; -- Imports the standard textio package.
+use std.textio.all;
 
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
---  Defines a design entity, without any ports.
 entity test is
 end test;
 
 architecture behaviour of test is
     constant NUMBER_OF_QUESTIONS    : Integer := 26;
-    constant COUNT_WIDTH            : Integer := 32;
+    constant COUNT_WIDTH            : Integer := 16;
     signal input_question           : std_logic_vector (1 to NUMBER_OF_QUESTIONS);
-    signal input_valid              : std_logic;
+    signal end_of_line              : std_logic;
+    signal end_of_group             : std_logic;
     signal output_value             : unsigned (COUNT_WIDTH - 1 downto 0); 
     signal clock                    : std_logic;
     signal reset                    : std_logic;
@@ -22,7 +21,8 @@ architecture behaviour of test is
         generic (NUMBER_OF_QUESTIONS    : Integer;
                  COUNT_WIDTH            : Integer);
         port (input_question            : in std_logic_vector (1 to NUMBER_OF_QUESTIONS);
-              input_valid               : in std_logic;
+              end_of_line               : in std_logic;
+              end_of_group              : in std_logic;
               output_value              : out unsigned (COUNT_WIDTH - 1 downto 0); 
               clock                     : in std_logic;
               reset                     : in std_logic);
@@ -33,7 +33,8 @@ begin
         generic map (NUMBER_OF_QUESTIONS => NUMBER_OF_QUESTIONS,
                      COUNT_WIDTH => COUNT_WIDTH)
         port map (input_question => input_question,
-                  input_valid => input_valid,
+                  end_of_line => end_of_line,
+                  end_of_group => end_of_group,
                   output_value => output_value,
                   clock => clock, reset => reset);
 
@@ -45,7 +46,8 @@ begin
     begin
         reset <= '1';
         clock <= '1';
-        input_valid <= '0';
+        end_of_group <= '0';
+        end_of_line <= '0';
         input_question <= (others => '0');
         wait for 10 ns;
 
@@ -57,6 +59,7 @@ begin
             readline (input_file, input_line);
 
             -- format the data as a 26-bit input
+            input_question <= (others => '0');
             for i in input_line'Range loop
                 v := Character'Pos (input_line (i));
                 v := v + 1 - Character'Pos ('a');
@@ -65,25 +68,26 @@ begin
                 end if;
             end loop;
 
+            end_of_group <= '0';
+            end_of_line <= '0';
+
             if input_line'Length = 0 then
                 -- reached the end of a group
-                input_valid <= '1';
+                end_of_group <= '1';
+            else
+                end_of_line <= '1';
             end if;
 
             clock <= '1';
             wait for 10 ns;
             clock <= '0';
             wait for 10 ns;
-
-            if input_line'Length = 0 then
-                -- reached the end of a group
-                input_question <= (others => '0');
-                input_valid <= '0';
-            end if;
-
         end loop;
-        -- reached the end of a group
-        input_valid <= '1';
+
+        -- reached the end of the final group
+        input_question <= (others => '0');
+        end_of_group <= '1';
+        end_of_line <= '0';
 
         clock <= '1';
         wait for 10 ns;
