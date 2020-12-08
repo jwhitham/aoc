fa7efb217965f97cd438cf0dbbf3d9192f095b6e
Jack Whitham <jack.d.whitham@gmail.com>
2020-12-08 10:31:38 +0000

    input generate

---
+++ 7/i2p.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -xe
+
+convert_i2p()
+{
+    sed -e 's/^\(.* bags*\) contain \(.* bags*\)\.$/data(\1,[\2])./' < $1 \
+            | tr ' ' '_' \
+            | sed -e 's/bags*/bag/g' \
+            | sed -e 's/_*[0-9][0-9]*_//g' > $1.pl
+}
+
+convert_i2p example_input
+
