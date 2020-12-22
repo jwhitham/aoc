2cc1823cbf71d7028e7e830b3f73769c4e8e1fc6
Jack Whitham <jack.d.whitham@gmail.com>
2020-12-22 12:04:26 +0000

    day 22 part 1

--- 22/aoc/Program.fs
+++ 22/aoc/Program.fs
@@ -1,8 +1,7 @@
 ﻿
 open System
 
-let test_player1_start = [9; 2; 6; 3; 1]
-let test_player2_start = [5; 8; 4; 7; 10]
+let readLines filePath = System.IO.File.ReadLines(filePath);;
 
 let rec calc_result = fun deck multiplier ->
         match deck with
@@ -10,29 +9,61 @@ let rec calc_result = fun deck multiplier ->
             ((h * multiplier) + (calc_result t (multiplier - 1)))
         | [] -> 0
 
-//val next_turn: deck1:[int] deck2:[int] int -> int int
-let rec next_turn = fun deck1 deck2 number ->
+let rec combat = fun deck1 deck2 ->
         match deck1 with
         | (head1 :: tail1) ->
             match deck2 with
             | (head2 :: tail2) ->
                 if (head1 > head2)
                 then
-                    next_turn (tail1 @ [head1; head2]) tail2 (number + 1)
+                    combat (tail1 @ [head1; head2]) tail2
                 else
-                    next_turn tail1 (tail2 @ [head2; head1]) (number + 1)
+                    combat tail1 (tail2 @ [head2; head1])
             | [] -> (1, (calc_result deck1 deck1.Length)) // player 1 wins
         | [] -> (2, (calc_result deck2 deck2.Length)) // player 2 wins
 
-let test =
-    let (winner, result) = (next_turn test_player1_start test_player2_start 1)
+let test_combat =
+    let test_player1_start = [9; 2; 6; 3; 1]
+    let test_player2_start = [5; 8; 4; 7; 10]
+    let (winner, result) = (combat test_player1_start test_player2_start)
     (winner = 2) && (result = 306)
+
+let parse_input =
+    let lines_seq = System.IO.File.ReadLines("input")
+    let lines_list = Seq.toList(lines_seq)
+
+    if (lines_list.Head = "Player 1:")
+    then
+        let rec get_deck_size = fun list ->
+            match list with
+            | ("" :: _) -> 0
+            | [] -> 0
+            | (_ :: t) -> (1 + get_deck_size t)
+        let rec string_to_int = fun list ->
+            match list with
+            | (h :: t) -> (Int32.Parse(h) :: string_to_int t)
+            | [] -> []
+
+        let deck_size = get_deck_size lines_list.Tail
+        let deck1 = lines_list.GetSlice(Some 1, Some deck_size)
+        let part2 = lines_list.GetSlice(Some (deck_size + 2), Some lines_list.Length)
+        if ((part2.Head = "Player 2:") && (deck_size = (get_deck_size part2.Tail)))
+        then
+            let deck2 = part2.GetSlice(Some 1, Some deck_size)
+            (string_to_int deck1, string_to_int deck2)
+        else
+            ([], [])
+    else
+        ([], [])
+
+
 
 [<EntryPoint>]
 let main argv =
-    if (test)
+    if (test_combat)
     then
-        let (winner, result) = (next_turn test_player1_start test_player2_start 1)
+        let (player1_start, player2_start) = parse_input
+        let (winner, result) = (combat player1_start player2_start)
         printfn "The winner is %d" winner
         printfn "The result is %d" result
         0
