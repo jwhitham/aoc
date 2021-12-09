90677759de2f61bb555b99b29f4f7a6d082d6506
Jack Whitham <jack.d.whitham@gmail.com>
2021-12-09 08:43:59 +0000

    day 9 part 2

--- 9/main.py
+++ 9/main.py
@@ -9,6 +9,7 @@ XY = typing.Tuple[int, int]
 class Height:
     def __init__(self, value: int) -> None:
         self.value = value
+        self.basin_id = 0
 
 class Heightmap:
     def __init__(self, filename: Path) -> None:
@@ -55,13 +56,49 @@ class Heightmap:
             risk += 1 + here.value
         return risk
 
+    def assign_basins(self) -> typing.List[int]:
+        basin_size: typing.Dict[int, int] = {}
+
+        def flood_fill(pos: XY) -> None:
+            target = self.get(pos)
+            if (not target) or target.basin_id or target.value >= 9:
+                return
+            target.basin_id = len(basin_size)
+            basin_size[target.basin_id] += 1
+            (x, y) = pos
+            flood_fill((x - 1, y))
+            flood_fill((x + 1, y))
+            flood_fill((x, y - 1))
+            flood_fill((x, y + 1))
+
+        for pos in self.find_low_points():
+            basin_size[len(basin_size) + 1] = 0
+            flood_fill(pos)
+
+        return sorted(basin_size.values())
+
+    def get_three_basins(self) -> int:
+        basins = self.assign_basins()
+        assert len(basins) >= 3
+        return basins[-1] * basins[-2] * basins[-3]
+
 def test_smoke_1() -> None:
     assert (1, 0) in Heightmap(Path("part1test.txt")).find_low_points()
     assert (2, 2) in Heightmap(Path("part1test.txt")).find_low_points()
     assert Heightmap(Path("part1test.txt")).get_risk_level_sum() == 15
 
+def test_smoke_2() -> None:
+    basins = Heightmap(Path("part1test.txt")).assign_basins()
+    assert len(basins) == 4
+    assert basins[-1] == 14
+    assert basins[-2] == 9
+    assert basins[-3] == 9
+    assert basins[-4] == 3
+    assert Heightmap(Path("part1test.txt")).get_three_basins() == 1134
+
 def main() -> None:
     print("part 1:", Heightmap(Path("part1.txt")).get_risk_level_sum())
+    print("part 2:", Heightmap(Path("part1.txt")).get_three_basins())
 
 if __name__ == "__main__":
     main()
