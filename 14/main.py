841b963721a20ea483af08e5f54fd6c8b6b0c22e
Jack Whitham <jack.d.whitham@gmail.com>
2021-12-14 11:48:09 +0000

    day 14 part 2

--- 14/main.py
+++ 14/main.py
@@ -1,6 +1,6 @@
 YEAR = 2021
 DAY = 14
-PART = 1
+PART = 3
  
 from pathlib import Path
 import unittest
@@ -12,42 +12,56 @@ import os
 
 INPUT = Path("input")
 
-def thing1(filename: Path) -> int:
-    with open(filename, "rt") as fd:
-        state = fd.readline().strip()
-        fd.readline()
-        match = dict()
-        for rule in fd:
-            (pair, outcome) = rule.strip().split(" -> ")
-            match[pair] = outcome
+Count = typing.Dict[str, int]
 
-        for step in range(10):
-            next_state = []
-            for i in range(len(state) - 1):
-                pair = state[i:i+2]
-                next_state.append(state[i])
-                if pair in match:
-                    next_state.append(match[pair])
-            next_state.append(state[-1])
-            state = ''.join(next_state)
+def factory() -> Count:
+    return collections.defaultdict(lambda: 0)
 
-        count: typing.Dict[str, int] = collections.defaultdict(lambda: 0)
-        for elt in state:
-            count[elt] += 1
+class Puzzle:
+    def __init__(self, filename: Path) -> None:
+        with open(filename, "rt") as fd:
+            self.original = fd.readline().strip()
+            fd.readline()
+            self.match: typing.Dict[str, str] = dict()
+            for rule in fd:
+                (pair, outcome) = rule.strip().split(" -> ")
+                self.match[pair] = outcome
 
-        lc_mc = sorted(count.values())
-        return lc_mc[-1] - lc_mc[0]
+        self.single_count = factory()
+        for i in range(len(self.original)):
+            self.single_count[self.original[i]] += 1
+
+        self.pair_count = factory()
+        for i in range(len(self.original) - 1):
+            self.pair_count[self.original[i:i+2]] += 1
+
+    def step(self) -> None:
+        # should have thought of this earlier...
+        after = factory()
+        before = self.pair_count
+        for old_pair in before:
+            if old_pair in self.match:
+                new_pair_1 = old_pair[0] + self.match[old_pair]
+                new_pair_2 = self.match[old_pair] + old_pair[1]
+                self.single_count[self.match[old_pair]] += before[old_pair]
+                after[new_pair_1] += before[old_pair]
+                after[new_pair_2] += before[old_pair]
+            else:
+                after[old_pair] += before[old_pair]
+
+        self.pair_count = after
+
+
+def stepper(filename: Path, steps: int) -> int:
+    p = Puzzle(filename)
+    for i in range(steps):
+        p.step()
+
+    lc_mc = sorted(p.single_count.values())
+    return lc_mc[-1] - lc_mc[0]
 
 def test_part_1() -> None:
-    assert thing1(Path("test_part_1")) == 1588
-
-def thing2(filename: Path) -> int:
-    return 0
-
-"""
-def test_part_2() -> None:
-    assert thing2(Path("test2")) == 1
-"""
+    assert stepper(Path("test_part_1"), 10) == 1588
 
 def main() -> None:
     if not INPUT.exists():
@@ -57,15 +71,9 @@ def main() -> None:
     subprocess.check_call([sys.executable, "-m", "mypy", sys.argv[0]])
     subprocess.check_call([sys.executable, "-m", "pytest", sys.argv[0]])
 
-    answer = thing1(INPUT)
+    answer = stepper(INPUT, 10)
     print("part 1:", answer)
-
-    if PART == 1:
-        subprocess.check_call(["aoc", "-y", str(YEAR), "-d", str(DAY),
-                               "submit", "1", str(answer)])
-        return
-
-    answer = thing2(INPUT)
+    answer = stepper(INPUT, 40)
     print("part 2:", answer)
 
     if PART == 2:
