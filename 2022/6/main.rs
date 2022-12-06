d7a8abb2e449a4c3c7b017cdf20996002cac3fe1
Jack Whitham <jack.d.whitham@gmail.com>
2022-12-06 10:35:34 +0000

    2022 day 6 part 2

--- 2022/6/main.rs
+++ 2022/6/main.rs
@@ -4,20 +4,20 @@ use std::io::{self, BufRead};
 use std::collections::VecDeque;
 use std::collections::HashMap;
 
-fn main() {
+fn part(size: usize) {
     let file = File::open("input").unwrap();
     let line = io::BufReader::new(file).lines().next().unwrap();
     let mut delay: VecDeque<char> = VecDeque::new();
     let mut ch_count: HashMap<char, u32> = HashMap::new();
-    let mut rx_count: u32 = 0;
+    let mut rx_count: usize = 0;
 
     for new_ch in line.expect("ok").chars() {
         delay.push_back(new_ch);
         ch_count.insert(new_ch, ch_count.get(&new_ch).unwrap_or(&0) + 1);
         rx_count += 1;
 
-        if rx_count > 4 {
-            assert_eq!(delay.len(), 5);
+        if rx_count > size {
+            assert_eq!(delay.len(), size + 1);
             let old_ch = delay.pop_front().unwrap();
             ch_count.insert(old_ch, ch_count.get(&old_ch).unwrap() - 1);
 
@@ -36,3 +36,8 @@ fn main() {
     }
 }
 
+
+fn main () {
+    part(4);
+    part(14);
+}
