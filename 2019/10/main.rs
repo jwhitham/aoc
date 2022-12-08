09715a0152919bc68add0222f16f8e486dfdf5e5
Jack Whitham <jack.d.whitham@gmail.com>
2022-12-08 12:09:10 +0000

    2022 day 8 part 2

--- 2019/10/main.rs
+++ 2019/10/main.rs
@@ -2,7 +2,6 @@
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::collections::HashSet;
-//use std::hash::{Hash, Hasher};
 
 
 type Word = i8;
@@ -13,16 +12,12 @@ struct Asteroid {
     y: Word,
 }
 
-/*
-impl Hash for Asteroid {
-    fn hash<H: Hasher>(&self, state: &mut H) {
-        self.x.hash(state);
-        self.y.hash(state);
-    }
-}
-*/
-
 fn greatest_common_divisor(a: Word, b: Word) -> Word {
+    if a == 0 {
+        return b;
+    } else if b == 0 {
+        return a;
+    }
     let mut copy_a = a;
     let mut copy_b = b;
     loop {
@@ -43,14 +38,12 @@ fn part1(filename: &str) -> u32 {
     let mut y: Word = 0;
     let mut x: Word = 0;
     let mut asteroid_set: HashSet<Asteroid> = HashSet::new();
-    let mut asteroid_list: Vec<Asteroid> = Vec::new();
     for line in io::BufReader::new(file).lines() {
         x = 0;
         if let Ok(line_string) = line {
             for ch in line_string.chars() {
                 if ch == '#' {
                     asteroid_set.insert(Asteroid { x: x, y: y });
-                    asteroid_list.push(Asteroid { x: x, y: y });
                 }
                 x += 1;
             }
@@ -60,39 +53,24 @@ fn part1(filename: &str) -> u32 {
     let width = x;
     let height = y;
     let mut best_visible_count: u32 = 0;
-    for centre in &asteroid_list {
+    for centre in &asteroid_set {
         // Count visible asteroids if the monitoring station is at centre
         let mut visible_count: u32 = 0;
-        //println!("try {},{}", centre.x, centre.y);
-        for asteroid in &asteroid_list {
+        for asteroid in &asteroid_set {
             // Here's the vector to the asteroid
             let mut dx = asteroid.x - centre.x;
             let mut dy = asteroid.y - centre.y;
-            if dx == 0 {
-                // The asteroid is directly along a vertical line
-                if dy == 0 {
-                    // Trivially visible! (same place)
-                } else if dy > 0 {
-                    dy = 1;
-                } else {
-                    dy = -1;
-                }
-            } else if dy == 0 {
-                // The asteroid is directly along a horizontal line
-                if dx > 0 {
-                    dx = 1;
-                } else {
-                    dx = -1;
-                }
-            } else {
-                // Reduce the vector to the smallest equivalent
-                let divide_by = Word::abs(greatest_common_divisor(dx, dy));
-                assert!((dx % divide_by) == 0);
-                assert!((dy % divide_by) == 0);
-                dx /= divide_by;
-                dy /= divide_by;
+            if dx == 0 && dy == 0 {
+                // Same place - does not count as visible
+                continue;
             }
-            //println!("  test {},{}  d {},{}", asteroid.x, asteroid.y, dx, dy);
+            // Reduce the vector to the smallest equivalent
+            let divide_by = Word::abs(greatest_common_divisor(dx, dy));
+            assert!((dx % divide_by) == 0);
+            assert!((dy % divide_by) == 0);
+            dx /= divide_by;
+            dy /= divide_by;
+
             // Step to the asteroid
             let mut sx = centre.x + dx;
             let mut sy = centre.y + dy;
@@ -106,20 +84,15 @@ fn part1(filename: &str) -> u32 {
                 if asteroid_set.contains(&Asteroid { x: sx, y: sy }) {
                     // The view is blocked!
                     visible = false;
-                    //println!("  blocked at {},{}", sx, sy);
                     break;
                 }
                 sx += dx;
                 sy += dy;
             }
             if visible {
-                // println!("  not blocked");
                 visible_count += 1;
             }
         }
-        // Can't see what you're standing on:
-        visible_count -= 1;
-        // println!("  count for {},{} is {}", centre.x, centre.y, visible_count);
         if visible_count > best_visible_count {
             best_visible_count = visible_count;
         }
--- 2022/8/main.rs
+++ 2022/8/main.rs
@@ -13,7 +13,10 @@ struct Location {
     y: Word,
 }
 
-fn part1(filename: &str) -> u32 {
+
+type Forest = HashMap<Location, Height>;
+
+fn read_input(filename: &str) -> (Forest, Word, Word) {
     let file = File::open(filename).unwrap();
     let mut y: Word = 0;
     let mut x: Word = 0;
@@ -28,8 +31,11 @@ fn part1(filename: &str) -> u32 {
         }
         y += 1;
     }
-    let width = x;
-    let height = y;
+    return (forest, x, y);
+}
+
+fn part1(filename: &str) -> u32 {
+    let (forest, width, height) = read_input(filename);
 
     fn scan_vector(forest: &HashMap<Location, Height>, start_loc: &Location,
                    dx: Word, dy: Word) -> bool {
@@ -70,8 +76,57 @@ fn test_part1() {
     assert_eq!(part1("test21"), 21);
 }
 
+fn part2(filename: &str) -> u32 {
+    let (forest, width, height) = read_input(filename);
+
+    fn scan_vector(forest: &HashMap<Location, Height>,
+                   start_loc: &Location,
+                   dx: Word, dy: Word) -> u32 {
+
+        let mut copy: Location = *start_loc;
+        let start_height = forest.get(&copy).unwrap();
+        let mut distance: u32 = 0;
+
+        loop {
+            copy.x += dx;
+            copy.y += dy;
+            if let Some (height) = forest.get(&copy) {
+                distance += 1;
+                if *height >= *start_height {
+                    // can't see beyond this
+                    return distance;
+                }
+            } else {
+                // reached the edge
+                return distance;
+            }
+        }
+    }
+
+    let mut best_score: u32 = 0;
+    for ty in 0 .. height {
+        for tx in 0 .. width {
+            let l = Location { x: tx, y: ty };
+            let c = scan_vector(&forest, &l, -1, 0)
+                * scan_vector(&forest, &l, 1, 0)
+                * scan_vector(&forest, &l, 0, -1)
+                * scan_vector(&forest, &l, 0, 1);
+            best_score = u32::max(best_score, c);
+        }
+    }
+
+    return best_score;
+}
+
+#[test]
+fn test_part2() {
+    assert_eq!(part2("test21"), 8);
+}
+
+
 fn main() {
     println!("{}", part1("input"));
+    println!("{}", part2("input"));
 }
 
 
