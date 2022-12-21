72b636b129bce50bb05d8fd1a6b6b6d621680bf6
Jack Whitham <jack.d.whitham@gmail.com>
2022-12-21 17:15:18 +0000

    2022 day 21 part 2

--- 2022/21/main.rs
+++ 2022/21/main.rs
@@ -5,6 +5,9 @@ use std::iter::FromIterator;
 use std::collections::HashMap;
 
 type Word = i64;
+const DEBUG: bool = false;
+const ROOT: &str = "root";
+const HUMN: &str = "humn";
 
 #[derive(Clone, Eq, PartialEq)]
 enum Node {
@@ -67,40 +70,52 @@ fn load(filename: &str) -> Problem {
     return p;
 }
 
-fn calc(p: &Problem, name: &String, cache: &mut Cache) -> Word {
-    let cached = cache.get(name);
+fn calc(p: &Problem, name: &String, cache: &mut Cache) -> Option<Word> {
+    let cached: Option<&Word> = cache.get(name);
     if cached.is_some() {
-        return *cached.unwrap();
+        return Some(*cached.unwrap());
     }
-    let node: &Node = p.get(name).unwrap();
+    let node: Option<&Node> = p.get(name);
+    if node.is_none() {
+        assert_eq!(name, &HUMN);
+        return None;
+    }
     let out: Word;
-    match node {
+    match node.unwrap() {
         Node::Literal(v) => {
             out = *v;
         },
         Node::Binary(op, a, b) => {
+            let av = calc(p, &a, cache);
+            if av.is_none() {
+                return None;
+            }
+            let bv = calc(p, &b, cache);
+            if bv.is_none() {
+                return None;
+            }
             match op {
                 Op::Add => {
-                    out = calc(p, a, cache) + calc(p, b, cache);
+                    out = av.unwrap() + bv.unwrap();
                 },
                 Op::Mul => {
-                    out = calc(p, a, cache) * calc(p, b, cache);
+                    out = av.unwrap() * bv.unwrap();
                 },
                 Op::Div => {
-                    out = calc(p, a, cache) / calc(p, b, cache);
+                    out = av.unwrap() / bv.unwrap();
                 },
                 Op::Sub => {
-                    out = calc(p, a, cache) - calc(p, b, cache);
+                    out = av.unwrap() - bv.unwrap();
                 },
             }
         },
     }
     cache.insert(name.to_string(), out);
-    return out;
+    return Some(out);
 }
 
 fn part1(p: &Problem) -> Word {
-    return calc(p, &"root".to_string(), &mut Cache::new());
+    return calc(p, &ROOT.to_string(), &mut Cache::new()).unwrap();
 }
 
 #[test]
@@ -108,38 +123,127 @@ fn test_part1() {
     assert_eq!(part1(&load(&"test")), 152);
 }
 
-fn part2(p: &Problem) -> Word {
-    let mut copy = p.clone();
-    let match_a: String;
-    let match_b: String;
-    match copy.get(&"root".to_string()).unwrap() {
+fn print_expr(p: &Problem, sub_root: &String) {
+    let found = p.get(sub_root);
+    if found.is_none() {
+        assert_eq!(sub_root, HUMN);
+        print!("{}", sub_root);
+        return;
+    }
+    match found.unwrap() {
+        Node::Literal(v) => {
+            print!("{}", v);
+        },
+        Node::Binary(op, a, b) => {
+            print!("(");
+            print_expr(p, a);
+            match op {
+                Op::Add => { print!(" + "); },
+                Op::Sub => { print!(" - "); },
+                Op::Mul => { print!(" * "); },
+                Op::Div => { print!(" / "); },
+            }
+            print_expr(p, b);
+            print!(")");
+        },
+    }
+}
+
+fn solve(p: &mut Problem, sub_root: &String, cache: &mut Cache,
+         equals: Word) -> Word {
+    if DEBUG {
+        print_expr(p, sub_root);
+        println!(" = {}", equals);
+    }
+    let rn: Option<&Node> = p.get(sub_root);
+    if rn.is_none() {
+        // Have reached humn = equals
+        assert_eq!(sub_root, HUMN);
+        return equals;
+    }
+    match rn.unwrap().clone() {
         Node::Literal(_) => {
+            // Must be a binary operation
             panic!();
         },
-        Node::Binary(_, a, b) => {
-            match_a = a.to_string(); match_b = b.to_string();
+        Node::Binary(op, ma, mb) => {
+            let va = calc(&p, &ma, cache);
+            let vb = calc(&p, &mb, cache);
+            if va.is_none() && vb.is_none() {
+                // Can't solve problems where "humn" appears on both sides
+                panic!();
+            } else if va.is_some() && vb.is_some() {
+                // Can't solve problems where "humn" doesn't appear
+                panic!();
+            }
+            match op {
+                Op::Sub => {
+                    if va.is_some() {
+                        // va - ? = equals
+                        // va = ? + equals
+                        // va - equals = ?
+                        return solve(p, &mb, cache, va.unwrap() - equals);
+                    } else {
+                        // ? - vb = equals
+                        // ? = vb + equals
+                        return solve(p, &ma, cache, vb.unwrap() + equals);
+                    }
+                },
+                Op::Div => {
+                    if va.is_some() {
+                        // va / ? = equals
+                        // va / equals = ?
+                        return solve(p, &mb, cache, va.unwrap() / equals);
+                    } else {
+                        // ? / vb = equals
+                        // ? = vb * equals
+                        return solve(p, &ma, cache, vb.unwrap() * equals);
+                    }
+                },
+                Op::Add => {
+                    if va.is_some() {
+                        // va + ? = equals
+                        return solve(p, &mb, cache, equals - va.unwrap());
+                    } else {
+                        // ? + vb = equals
+                        return solve(p, &ma, cache, equals - vb.unwrap());
+                    }
+                },
+                Op::Mul => {
+                    if va.is_some() {
+                        // va * ? = equals
+                        return solve(p, &mb, cache, equals / va.unwrap());
+                    } else {
+                        // ? * vb = equals
+                        return solve(p, &ma, cache, equals / vb.unwrap());
+                    }
+                },
+            }
         },
     }
-    copy.remove(&"root".to_string());
-    let mut cache_a = Cache::new();
-    calc(&copy, &match_a, &mut cache_a);
-    let mut cache_b = Cache::new();
-    calc(&copy, &match_b, &mut cache_b);
-    if cache_a.contains_key(&"humn".to_string()) {
-        println!("cache a!");
+}
+    
+fn part2(p: &mut Problem) -> Word {
+    p.remove(&HUMN.to_string());
+    match p.remove(&ROOT.to_string()).unwrap() {
+        Node::Literal(_) => {
+            // Root must be a binary operation
+            panic!();
+        },
+        Node::Binary(_, ma, mb) => {
+            // Rewrite root as A - B so that we can solve A - B = 0
+            p.insert(ROOT.to_string(), Node::Binary(Op::Sub, ma, mb));
+        },
     }
-    if cache_a.contains_key(&"humn".to_string()) {
-        println!("cache b!");
-    }
-    return 0;
+    return solve(p, &ROOT.to_string(), &mut Cache::new(), 0);
 }
 
 #[test]
 fn test_part2() {
-    assert_eq!(part2(&load(&"test")), 301);
+    assert_eq!(part2(&mut load(&"test")), 301);
 }
 
 fn main() {
     println!("{}", part1(&load(&"input")));
-    println!("{}", part2(&load(&"input")));
+    println!("{}", part2(&mut load(&"input")));
 }
