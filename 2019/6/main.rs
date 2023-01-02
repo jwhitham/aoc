6adf81bddec845d5a148f0e87f604d78f61d6c01
Jack Whitham <jack.d.whitham@gmail.com>
2023-01-02 18:47:25 +0000

    2019 day 6 rewrite without Rc/RefCell

--- 2019/6/main.rs
+++ 2019/6/main.rs
@@ -2,19 +2,15 @@
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::collections::HashMap;
-use std::rc::{Rc, Weak};
-use std::cell::{RefMut, RefCell};
-use std::borrow::Borrow;
 
 
 struct Object {
-    parent: Option<Weak<RefCell<Object>>>,
-    children: Vec<Weak<RefCell<Object>>>,
+    parent: String,
     flag: bool,
 }
 
 struct Space {
-    object: HashMap<String, Rc<RefCell<Object>>>,
+    object: HashMap<String, Object>,
 }
 
 impl Space {
@@ -24,43 +20,47 @@ impl Space {
         };
     }
 
-    fn get(self: &mut Self, name: &str) -> Rc<RefCell<Object>> {
-        if let Some (obj) = self.object.get(name) {
-            return obj.clone();
+    fn create(self: &mut Self, name: &str) {
+        if !self.object.contains_key(name) {
+            self.object.insert(name.to_string(), Object {
+                parent: String::new(),
+                flag: false,
+            });
         }
-        let new_obj = Rc::new(RefCell::new(Object {
-            parent: None,
-            children: Vec::new(),
-            flag: false,
-        }));
-        self.object.insert(name.to_string(), new_obj.clone());
-        return new_obj;
+    }
+
+    fn get(self: &Self, name: &str) -> Option<&Object> {
+        return self.object.get(name);
+    }
+
+    fn get_mut(self: &mut Self, name: &str) -> Option<&mut Object> {
+        return self.object.get_mut(name);
+    }
+
+    fn parent_name(self: &Self, name: &str) -> Option<String> {
+        return self.get(name).map(|item| item.parent.clone());
     }
 
     fn add_link(self: &mut Self,
                 planet_name: &str, satellite_name: &str) {
         
-        let planet = self.get(planet_name);
-        let satellite = self.get(satellite_name);
-        let mut planet_ref: RefMut<Object> = planet.borrow_mut();
-        let mut satellite_ref: RefMut<Object> = satellite.borrow_mut();
-        assert!(satellite_ref.parent.is_none());
-        satellite_ref.parent = Some(Rc::downgrade(&planet));
-        planet_ref.children.push(Rc::downgrade(&satellite));
+        self.create(planet_name);
+        self.create(satellite_name);
+        if let Some(satellite) = self.get_mut(satellite_name) {
+            satellite.parent = planet_name.to_string();
+        }
     }
 
     fn count_transitive(self: &mut Self) -> u32 {
         let mut count = 0;
-        for obj in self.object.values() {
-            let satellite: &Rc<RefCell<Object>> = obj;
-            let obj_ref: &RefCell<Object> = satellite.borrow();
-            let mut next_step = obj_ref.borrow().parent.clone();
-            while next_step.is_some() {
+        for satellite_name in self.object.keys() {
+            let mut name = self.parent_name(satellite_name);
+            if name.is_some() {
+                name = self.parent_name(&name.unwrap());
+            }
+            while name.is_some() {
                 count += 1;
-                let step_weak_ref: Weak<RefCell<Object>> = next_step.unwrap();
-                let step_ref: Rc<RefCell<Object>> = step_weak_ref.upgrade().unwrap();
-                let step: &RefCell<Object> = step_ref.borrow();
-                next_step = step.borrow().parent.clone();
+                name = self.parent_name(&name.unwrap());
             }
         }
         return count;
@@ -68,20 +68,19 @@ impl Space {
 
     fn flip_flag(self: &mut Self, satellite_name: &str) -> u32 {
         let mut count = 0;
-        let satellite = self.get(satellite_name);
-        let obj_ref: &RefCell<Object> = satellite.borrow();
-        let mut next_step = obj_ref.borrow().parent.clone();
-        while next_step.is_some() {
-            let step_weak_ref: Weak<RefCell<Object>> = next_step.unwrap();
-            let step_ref: Rc<RefCell<Object>> = step_weak_ref.upgrade().unwrap();
-            let step: &RefCell<Object> = step_ref.borrow();
-            if !step.borrow().flag {
-                count += 1;
-                step.borrow_mut().flag = true;
+        let mut name = self.parent_name(satellite_name);
+        while name.is_some() {
+            if let Some(mut value) = self.get_mut(&name.unwrap()) {
+                if !value.flag {
+                    count += 1;
+                    value.flag = true;
+                } else {
+                    value.flag = false;
+                }
+                name = Some(value.parent.clone());
             } else {
-                step.borrow_mut().flag = false;
+                name = None;
             }
-            next_step = step.borrow().parent.clone();
         }
         return count;
     }
@@ -102,23 +101,29 @@ fn load_from_input() -> Space {
     return space;
 }
 
-fn part1() {
+fn part1() -> u32 {
     let mut space = load_from_input();
-    println!("{}", space.count_transitive());
+    return space.count_transitive();
 }
 
-fn part2() {
+fn part2() -> u32 {
     let mut space = load_from_input();
     let you_to_root = space.flip_flag("YOU");
     let santa_to_meeting = space.flip_flag("SAN");
     let meeting_to_root = space.flip_flag("YOU");
     let you_to_meeting = you_to_root - meeting_to_root;
     let you_to_santa = you_to_meeting + santa_to_meeting;
-    println!("{}", you_to_santa);
+    return you_to_santa;
+}
+
+#[test]
+fn test() {
+    assert_eq!(139597, part1());
+    assert_eq!(286, part2());
 }
 
 fn main() {
-    part1();
-    part2();
+    println!("{}", part1());
+    println!("{}", part2());
 }
 
