f1cb4850c8c773fd5e8a9dac79408f63904361e2
Jack Whitham <jack.d.whitham@gmail.com>
2024-01-09 01:27:09 +0000

    2023 day 24 part 2 wip

--- 2023/24/main.py
+++ 2023/24/main.py
@@ -4,6 +4,17 @@ import re
 
 IntersectXY = typing.Optional[typing.Tuple[float, float]]
 DEBUG = False
+EPSILON = 1e-6
+
+
+def near(a: float, b: float) -> bool:
+    return abs(a - b) < EPSILON
+
+def near_or_none(a: typing.Optional[float], b: typing.Optional[float]) -> bool:
+    if (a is not None) and (b is not None):
+        return near(a, b)
+    else:
+        return True
 
 class Stone:
     def __init__(self,
@@ -76,10 +87,57 @@ class Problem:
 
         return total
 
+    def part2(self) -> int:
+        ti = 1
+        for tj in range(2, 20):
+            for i in range(len(self.stones)):
+                for j in range(len(self.stones)):
+                    #print("try", i, ti, j, tj)
+                    v = self.try_vector(i, ti, j, tj)
+                    if v is not None:
+                        return v
+
+        # Not solveable
+        return -1
+        
+    def try_vector(self, i: int, ti: int, j: int, tj: int) -> typing.Optional[int]:
+        # Determine vector that passes through i at ti, then j at tj
+        # pxi + ti*vxi = pxa + ti*vxa   AND   pxj + tj*vxj = pxa + tj*vxa
+        # pxi + ti*vxi - pxj - tj*vxj = pxa + ti*vxa - pxa - tj*vxa
+        # pxi + ti*vxi - pxj - tj*vxj = (ti - tj)*vxa
+        vxa = (self.stones[i].px + (ti * self.stones[i].vx)
+                - self.stones[j].px - (tj * self.stones[j].vx)) / (ti - tj)
+        vya = (self.stones[i].py + (ti * self.stones[i].vy)
+                - self.stones[j].py - (tj * self.stones[j].vy)) / (ti - tj)
+        vza = (self.stones[i].pz + (ti * self.stones[i].vz)
+                - self.stones[j].pz - (tj * self.stones[j].vz)) / (ti - tj)
+        pxa = self.stones[i].px + (ti * self.stones[i].vx) - (ti * vxa)
+        pya = self.stones[i].py + (ti * self.stones[i].vy) - (ti * vya)
+        pza = self.stones[i].pz + (ti * self.stones[i].vz) - (ti * vza)
+        #print("try", i, j, pxa, pya, pza)
+
+        # Does this work?
+        for k in range(len(self.stones)):
+            # pxk + tk*vxk = pxa + tk*vxa
+            # (pxk - pxa) / (vxa - vxk) = tk
+            tkx = tky = tkz = None
+            if not near(vxa, self.stones[k].vx):
+                tkx = (self.stones[k].px - pxa) / (vxa - self.stones[k].vx)
+            if not near(vya, self.stones[k].vy):
+                tky = (self.stones[k].py - pya) / (vya - self.stones[k].vy)
+            if not near(vza, self.stones[k].vz):
+                tkz = (self.stones[k].pz - pza) / (vza - self.stones[k].vz)
+            if not (near_or_none(tkx, tky) and near_or_none(tky, tkz)
+                    and near_or_none(tkx, tkz)):
+                return None
+
+        return pxa + pya + pza
+
+
 if __name__ == "__main__":
     assert Problem("test").part1(7, 27) == 2
     bound1 = 200000000000000
     bound2 = 400000000000000
     print(Problem("input").part1(bound1, bound2))
-
-
+    assert Problem("test").part2() == 47
+    #print(Problem("input").part2())
